18e49add08477e1c7d77bcbfd55736e78c028bb8
Henrique Silveira <henrique.silveira@tech4h.com.br>
2022-11-27 16:12:37 -0300

    Corrections Duplication

--- cypress/integration/duplication/sample4.spec.js
+++ cypress/integration/duplication/sample4.spec.js
@@ -2,10 +2,8 @@ describe('Code duplication bad practice - Sample 4', () => {
   beforeEach(() => {
     cy.visit('https://bit.ly/2XSuwCW')
   })
- 
+
   it('checks all checkboxes from a specific fieldset', () => {
-    cy.get('#friend').check()
-    cy.get('#publication').check()
-    cy.get('#social-media').check()
+    cy.get('fieldset div input[type="checkbox"]').check()
   })
 })
