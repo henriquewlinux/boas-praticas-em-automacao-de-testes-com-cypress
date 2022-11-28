72af3e58e5e8be1e8c3a150105e4723cbcb86a0d
Henrique Silveira <henrique.silveira@tech4h.com.br>
2022-11-28 20:36:51 -0300

    Corrections HardCoded

---
+++ cypress/integration/hardcoded-assertion/sample1.spec.js
@@ -0,0 +1,23 @@
+describe('Hardcoded assertion bad practice', () => {
+  beforeEach(() => {
+    cy.intercept('GET', '**/search**', { fixture: 'stories' }).as('getStories')
+
+    cy.visit('https://hackernews-seven.vercel.app')
+    cy.wait('@getStories')
+  })
+
+  it('searches', () => {
+
+    cy.search('cypress.io')
+    cy.wait('@getStories')
+
+    cy.fixture('stories').then(stories => {
+      cy.get('.table-row')
+        .as('tableRows')
+        .should('have.length', stories.hits.length)
+      stories.hits.forEach((hit, index) => {
+        cy.get('@tableRows').eq(index).should('contain', hit.title)
+      })
+    })
+  })
+})
