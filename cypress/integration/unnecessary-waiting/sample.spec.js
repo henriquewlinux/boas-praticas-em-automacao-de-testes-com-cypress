8cc81c03c279f71aa2a9d561c22805dceced4eb1
Henrique Silveira <henrique.silveira@tech4h.com.br>
2022-11-28 22:14:13 -0300

    Corrections Unnecessary-Wait

--- cypress/integration/unnecessary-waiting/sample.spec.js
+++ cypress/integration/unnecessary-waiting/sample.spec.js
@@ -4,7 +4,9 @@ describe('Unnecessary waiting bad practice', () => {
   const randomDestination = Math.floor(Math.random() * 15) + 1
 
   beforeEach(() => {
-    cy.visit(`https://lit-chamber-61567.herokuapp.com/destinations/${randomDestination}/edit`)
+    cy.visit(
+      `https://lit-chamber-61567.herokuapp.com/destinations/${randomDestination}/edit`
+    )
   })
 
   it('updates destination info', () => {
@@ -13,24 +15,16 @@ describe('Unnecessary waiting bad practice', () => {
       description: faker.random.words(5)
     }
 
-    cy.get('#destination_name')
-      .clear()
-      .type(info.name)
-    cy.get('#destination_description')
-      .clear()
-      .type(info.description)
-    cy.get('input[type="submit"]')
-      .click()
-    cy.wait(3000)
+    cy.get('#destination_name').clear().type(info.name)
+    cy.get('#destination_description').clear().type(info.description)
+    cy.get('input[type="submit"]').click()
 
-    cy.url()
-      .should(
-        'be.equal',
-        `https://lit-chamber-61567.herokuapp.com/destinations/${randomDestination}`
-      )
-    cy.contains('h2', info.name)
-      .should('be.visible')
-    cy.contains('p', info.description)
-      .should('be.visible')
+    cy.url().should(
+      'be.equal',
+      `https://lit-chamber-61567.herokuapp.com/destinations/${randomDestination}`,
+      { timeout: 10000 }
+    )
+    cy.contains('h2', info.name).should('be.visible')
+    cy.contains('p', info.description).should('be.visible')
   })
 })
